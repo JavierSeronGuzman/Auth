7e1e12c715068af66b600889bb4689fb67c8f018
JavierSeronGuzman <seron.jsg@gmail.com>
2024-05-19 08:35:28 -0400

    Ready

---
+++ src/main/java/com/example/auth/services/AuthService.java
@@ -0,0 +1,10 @@
+package com.example.auth.services;
+
+import com.example.auth.common.dto.TokenResponse;
+import com.example.auth.common.dto.UserRequest;
+
+public interface AuthService {
+    TokenResponse createUser(UserRequest userRequest);
+
+    TokenResponse login(UserRequest userRequest);
+}
